b6c8dbbc2fc4057c95cb66549fe6564cc11e159e
zaiyin <riyuzakikira@gmail.com>
2026-05-23 16:46:07 +0700

    Update prx.py

--- prx.py
+++ prx.py
@@ -8,8 +8,8 @@ from bs4 import BeautifulSoup
 # ============================================
 # COUNTRY LIST
 # ============================================
-COUNTRY = ["sg", "my", "jp"]
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+COUNTRY = ["id", "sg", "my", "jp", "us"]
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
 # ============================================
 # AUTO AMBIL SUBSCRIPTION URL
